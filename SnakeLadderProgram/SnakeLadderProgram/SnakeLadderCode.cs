73c0b45a7aa86d1bca2a03b3c39a19d7868d71f3
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-08 21:40:23 +0530

    [Anjaneya]Added:Initialisation of player1

---
+++ SnakeLadderProgram/SnakeLadderProgram/SnakeLadderCode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadderProgram
+{
+    class SnakeLadderCode
+    {
+        public void Start()
+        {
+            int Player1Position = 0;
+        }
+    }
+}
